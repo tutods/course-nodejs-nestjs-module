eb2ca94a81a0894cada8c66c3628db0f989147a4
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-01-13 22:57:26 +0000

    feat(wip): setup coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,10 +2,26 @@ import swc from 'unplugin-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
+const config = defineConfig({
   test: {
     globals: true,
     root: './',
+    coverage: {
+      enabled: false,
+      provider: 'v8',
+      include: ['src/**/*.ts'],
+      all: false,
+      clean: true,
+      cleanOnRerun: true,
+      thresholds: {
+        perFile: true,
+        autoUpdate: true,
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80,
+      },
+    },
   },
   plugins: [
     tsconfigPaths(),
@@ -16,3 +32,5 @@ export default defineConfig({
     }),
   ],
 });
+
+export default config;
