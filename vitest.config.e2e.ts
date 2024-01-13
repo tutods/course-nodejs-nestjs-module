0a2fb2af22e4238c650ba2763a574bd631089e2c
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-01-13 22:41:42 +0000

    refactor: use swc and vitest

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,12 @@
+import swc from 'unplugin-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    root: './',
+  },
+  plugins: [tsconfigPaths(), swc.vite()],
+});
---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import swc from 'unplugin-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    root: './',
+  },
+  plugins: [
+    tsconfigPaths(),
+    // This is required to build the test files with SWC
+    swc.vite({
+      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
+      module: { type: 'es6' },
+    }),
+  ],
+});
